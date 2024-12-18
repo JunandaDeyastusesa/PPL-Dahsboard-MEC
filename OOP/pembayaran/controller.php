88e14f4a06452673e6dda7acce2d95e6fecefb4f
Zky-Jw <mozabang1@gmail.com>
2024-12-19 01:41:42 +0700

    Sprint-3 Daily 4 Update & Delete

--- OOP/pembayaran/controller.php
+++ OOP/pembayaran/controller.php
@@ -41,18 +41,7 @@ class Controller extends koneksi
         }
         return $perintah;
     }
-    public function ViewRelasiBayar()
-    {
-        $_ViewRelasi = "SELECT * FROM table_pembayaran 
-                        INNER JOIN table_siswa ON table_pembayaran.id_siswa = table_siswa.id_siswa
-                        INNER JOIN table_paket_kls ON table_pembayaran.id_kelas = table_paket_kls.id_kelas ORDER BY id_bayar ASC";
-        $perintah = $this->query($_ViewRelasi);
-        if (!$perintah) {
-            die("Error : " . $this->koneksi->error);
-        }
-        return $perintah;
-    }
-}
+
     public function ViewById($data)
     {
         $_ViewById = "SELECT * FROM table_pembayaran WHERE id_bayar=?";
@@ -109,4 +98,54 @@ class Controller extends koneksi
         }
     }
 
-    
+    public function Edit($id_bayar, $tanggal, $id_siswa, $id_kelas)
+    {
+        $_Edit = "UPDATE table_pembayaran SET tanggal=?, id_siswa=?, id_kelas=? WHERE id_bayar=?";
+        if ($_statement = $this->prepare($_Edit)) {
+            $_statement->bind_param("sssi", $param_tanggal, $param_id_siswa, $param_id_kelas, $param_id_bayar);
+
+            $param_id_bayar = $id_bayar;
+            $param_tanggal = $tanggal;
+            $param_id_siswa = $id_siswa;
+            $param_id_kelas = $id_kelas;
+
+            if ($_statement->execute()) {
+                echo "<script> alert('Data berhasil diubah!'); window.location='index.php'; </script>";
+                // $_statement->close();
+                exit();
+            } else {
+                return false;
+            }
+        }
+        $_statement->close();
+    }
+
+    public function DeleteById($id_bayar)
+    {
+        $_DeleteByid_bayar = "DELETE FROM table_pembayaran WHERE id_bayar=?";
+        if ($_statement = $this->prepare($_DeleteByid_bayar)) {
+            $_statement->bind_param("i", $param_id_bayar);
+            $param_id_bayar = $id_bayar;
+            if ($_statement->execute()) {
+                echo "<script> alert('Data Telah Dihapus!'); window.location='index.php'; </script>";
+                $_statement->close();
+                exit();
+            } else {
+                return false;
+            }
+        }
+        $_statement->close();
+    }
+
+    public function ViewRelasiBayar()
+    {
+        $_ViewRelasi = "SELECT * FROM table_pembayaran 
+                        INNER JOIN table_siswa ON table_pembayaran.id_siswa = table_siswa.id_siswa
+                        INNER JOIN table_paket_kls ON table_pembayaran.id_kelas = table_paket_kls.id_kelas ORDER BY id_bayar ASC";
+        $perintah = $this->query($_ViewRelasi);
+        if (!$perintah) {
+            die("Error : " . $this->koneksi->error);
+        }
+        return $perintah;
+    }
+}
